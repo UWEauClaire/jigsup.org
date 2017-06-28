95d0ee5a907df96022649b93d8d0972ed6157447
yenser <jens.streck@gmail.com>
2017-06-28 13:19:19 -0500

    Past Results FINISHED
    finished converting all tables to JSON and having page load past results.

---
+++ scripts/tableBuilder.js
@@ -0,0 +1,118 @@
+//2017
+getFishWinnerFile("JSON/2017FishPrizeWinner.json", "2017FishPrizeWinner");
+getFishWinnerFile("JSON/2017RaffleWinner.json", "2017RaffleWinner");
+getSpeciesWinnerFile("JSON/2017SpeciesWinner.json", "2017SpeciesWinner");
+
+//2016
+getFishWinnerFile("JSON/2016FishPrizeWinner.json", "2016FishPrizeWinner");
+get2016RaffleFile("JSON/2016RaffleWinner.json", "2016RaffleWinner");
+
+//2015
+getFishWinnerFile("JSON/2015FishPrizeWinner.json", "2015FishPrizeWinner");
+get2015RaffleFile("JSON/2015RaffleWinner.json", "2015RaffleWinner");
+
+//2014
+getFishWinnerFile("JSON/2014FishPrizeWinner.json", "2014FishPrizeWinner");
+
+
+
+
+
+
+
+
+
+
+
+function getFishWinnerFile(fileLocation, tableBodyName) {
+  $.getJSON(fileLocation, function(data) {})
+   .done(function(data) {
+      for(i = 0; i < data.winners.length; i++) {
+        var text = "<tr>"
+        + "<td>" + data.winners[i].pos + "</td>"
+        + "<td>" + data.winners[i].name + "</td>"
+        + "<td>" + data.winners[i].weight + "</td>"
+        + "<td>" + data.winners[i].species + "</td>"
+        + "<td>" + data.winners[i].prize + "</td>"
+        +"</tr>";
+        $('#'+tableBodyName).append(text);
+      }
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
+}
+
+function get2017RaffleFile(fileLocation, tableBodyName) {
+  $.getJSON(fileLocation, function(data) {})
+   .done(function(data) {
+      for(i = 0; i < data.winners.length; i++) {
+        var text = "<tr>"
+        + "<td>" + data.winners[i].prize + "</td>"
+        + "<td>" + data.winners[i].name + "</td>"
+        +"</tr>";
+        $('#'+tableBodyName).append(text);
+      }
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
+}
+
+function getSpeciesWinnerFile(fileLocation, tableBodyName) {
+  $.getJSON(fileLocation, function(data) {})
+   .done(function(data) {
+      for(i = 0; i < data.winners.length; i++) {
+        var text = "<tr>"
+        + "<td>" + data.winners[i].name + "</td>"
+        + "<td>" + data.winners[i].weight + "</td>"
+        + "<td>" + data.winners[i].species + "</td>"
+        + "<td>" + data.winners[i].prize + "</td>"
+        +"</tr>";
+        $('#'+tableBodyName).append(text);
+      }
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
+}
+
+function get2016RaffleFile(fileLocation, tableBodyName) {
+  $.getJSON(fileLocation, function(data) {})
+   .done(function(data) {
+      for(i = 0; i < data.winners.length; i++) {
+        var text = "<tr>"
+        + "<td>" + data.winners[i].pos + "</td>"
+        + "<td>" + data.winners[i].name + "</td>"
+        + "<td>" + data.winners[i].prize + "</td>"
+        + "<td>" + data.winners[i].sponsor + "</td>"
+        +"</tr>";
+        $('#'+tableBodyName).append(text);
+      }
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
+}
+
+function get2015RaffleFile(fileLocation, tableBodyName) {
+  $.getJSON(fileLocation, function(data) {})
+   .done(function(data) {
+      for(i = 0; i < data.winners.length; i++) {
+        var text = "<tr>"
+        + "<td>" + data.winners[i].pos + "</td>"
+        + "<td>" + data.winners[i].name + "</td>"
+        + "<td>" + data.winners[i].prize + "</td>"
+        +"</tr>";
+        $('#'+tableBodyName).append(text);
+      }
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
+}
--- scripts/tableTest.js
+++ scripts/tableTest.js
@@ -1,24 +1,27 @@
-const fs = require('fs');
+getJSONFile("JSON/2017FishPrizeWinner.json");
 
-var fetchNotes = () => {
-  try {
-    var notesString = fs.readFileSync('2017FishPrizeWinner.json');
-    return JSON.parse(notesString);
-  } catch (e) {
-    return [];
-  }
-};
 
-var saveNotes = (notes) => {
-  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-};
 
-var getAll = () => {
-  return fetchNotes();
-};
+function getJSONFile(fileLocation) {
+  $.getJSON(fileLocation, function(data) {})
+   .done(function(data) {
+      // document.write( "JSON Data: " + JSON.stringify(json, undefined, 2) );
+      //alert(JSON.stringify(data, undefined, 2));
 
-var logNote = (note) => {
-  console.log('--');
-  console.log(`pos: ${note.pos}`);
-  console.log(`name: ${note.name}`);
-};
+      for(i = 0; i < data.winners.length; i++) {
+        var text = "<tr>"
+        + "<td>" + data.winners[i].pos + "</td>"
+        + "<td>" + data.winners[i].name + "</td>"
+        + "<td>" + data.winners[i].weight + "</td>"
+        + "<td>" + data.winners[i].species + "</td>"
+        + "<td>" + data.winners[i].prize + "</td>"
+        +"</tr>";
+        $('#tbody').append(text);
+      }
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ", " + error;
+      console.log( "Request Failed: " + err );
+  });
+
+}
