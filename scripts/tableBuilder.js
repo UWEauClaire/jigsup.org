87d18629c532e27726b0aa9f9df6478b98279333
Joel Schilling <Joel1540@gmail.com>
2018-11-13 12:59:26 -0600

    launch of updated site with bootstrap 4

--- scripts/tableBuilder.js
+++ scripts/tableBuilder.js
@@ -8,11 +8,11 @@ getFishWinnerFile("../JSON/2016FishPrizeWinner.json", "2016FishPrizeWinner");
 get2016RaffleFile("../JSON/2016RaffleWinner.json", "2016RaffleWinner");
 
 //2015
-getFishWinnerFile("../JSON/2015FishPrizeWinner.json", "2015FishPrizeWinner");
-get2015RaffleFile("../JSON/2015RaffleWinner.json", "2015RaffleWinner");
+//getFishWinnerFile("../JSON/2015FishPrizeWinner.json", "2015FishPrizeWinner");
+//get2015RaffleFile("../JSON/2015RaffleWinner.json", "2015RaffleWinner");
 
 //2014
-getFishWinnerFile("../JSON/2014FishPrizeWinner.json", "2014FishPrizeWinner");
+//getFishWinnerFile("../JSON/2014FishPrizeWinner.json", "2014FishPrizeWinner");
 
 
 
